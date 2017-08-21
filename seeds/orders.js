f7b753e582f9b58f11cb7035082111ab13b7b762
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 16:22:30 +1200

    Add BatMan

--- seeds/orders.js
+++ seeds/orders.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
         {id: 1, order_name: 'Joe', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: true, paid: true, },
         {id: 2, order_name: 'Betty', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: true, paid: true, },
         {id: 3, order_name: 'Patrick', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: true, paid: true, },
-        {id: 4, order_name: 'Camel', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: true, paid: true, },
+        {id: 4, order_name: 'BatMan', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: true, paid: true, },
         {id: 5, order_name: 'Ajax', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: false, paid: true, },
         {id: 6, order_name: 'Patrick', created: '16:21 21-Aug-17', pickup_time: '16:30 21-Aug-17', completed: false, paid: true, }
       ]);
