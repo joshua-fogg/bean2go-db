b3dd299bc058071215a9491b998b8e8659270abf
Joshua <joshuafoggnz@gmail.com>
2017-08-22 10:19:34 +1200

    reconfiguring server layout

--- server/server.js
+++ server/server.js
@@ -5,13 +5,12 @@ var path = require('path')
 var config = require('./knexfile').development
 var db = require('knex')(config)
 
-const html = require('../public/index.html') // requiring index.html
 const apiRoutes = require('./api-routes') // directing to api-routes for api services.
 
 server.use(express.static(path.join(__dirname, 'public'))) // setting server public folder
 server.use('/api/v1', apiRoutes) // parsing api calls to api-routes
 server.get('*', (req, res) => { // wildcard server route to send index && app
-    res.send(html)
+    res.send('../public/index.html')
 })
 
 server.listen(PORT, function() {
