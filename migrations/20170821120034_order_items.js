33258d714270a9fcd8614801cdc78020ce4ab68e
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 12:23:01 +1200

    Create  order_items schema

--- migrations/20170821120034_order_items.js
+++ migrations/20170821120034_order_items.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTableIfNotExtsis('order_items', (t) => {
+        t.increments('id')
+        t.integer('order_id').references('orders.id')
+        t.integer('coffee_id').references('coffee_list.id')
+        t.string('person_name')
+
+    })
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('order_items')
   
 };
