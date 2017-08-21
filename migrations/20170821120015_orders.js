9cc56e094f02ab44c1c996e2e0253cfdbee0ff78
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 12:16:09 +1200

    Create orders schema

--- migrations/20170821120015_orders.js
+++ migrations/20170821120015_orders.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTableIfNotExists('orders', (t) => {
+        t.increment('id')
+        t.string('order_name')
+        t.string('created_id')
+        t.string('pickup_time')
+        t.boolean('completed')
+        t.boolean('paid')
+    })
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('orders')
   
 };
