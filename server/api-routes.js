04b37d2d4dbc95ca4443094599fe838d990b951d
Joshua <joshuafoggnz@gmail.com>
2017-08-22 10:59:54 +1200

    building server run

--- server/api-routes.js
+++ server/api-routes.js
@@ -1,8 +1,11 @@
-var express = require('express')
-var server = express()
-var path = require('path')
+const express = require('express')
+const server = express()
+const path = require('path')
+const knex = require('knex')
+const config = require('../knexfile').development
+var db = require('knex')(config)
 
-
-server.use('/', (req, res) => {
-
+server.get('/getlist', (req, res) => {
+    let coffeeList = db('coffee_list').select('*')
+    res.json(coffeeList)
 })
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+var path = require('path')
 var express = require('express')
 var server = express()
-var path = require('path')
 
+
+const PORT = process.env.PORT || 3000 // establish PORT, add deployment port ||
 const apiRoutes = require('./api-routes') // directing to api-routes for api services.
 
-server.use(express.static(path.join(__dirname, 'public'))) // setting server public folder
-server.use('/api/v1', apiRoutes) // parsing api calls to api-routes
-server.get('*', (req, res) => { // wildcard server route to send index && app
-    res.send('../public/index.html')
-})
 
-server.listen(PORT, function() {
-    console.log('Listening on port', PORT)
+server.listen(PORT, function() { console.log('Listening on port', PORT) })
+server.use(express.static(path.join(__dirname, 'public'))) // setting server public folder
+
+
+server.use('/api/v1', apiRoutes) // parsing api calls to api-routes
+server.use('*', (req, res) => { // wildcard server route to send index && app
+    res.send('../public/index.html')
 })
