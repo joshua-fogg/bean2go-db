dabc38cbe9acbdf32972831558724e9e08d18075
Joshua <joshuafoggnz@gmail.com>
2017-08-22 10:16:47 +1200

    remove doubked up created time

--- migrations/2_orders.js
+++ migrations/2_orders.js
@@ -1,18 +1,16 @@
-
 exports.up = function(knex, Promise) {
     return knex.schema.createTableIfNotExists('orders', (t) => {
         t.increment('id')
         t.string('order_name')
-        t.string('created')
-        t.string('pickup_time')
         t.timestamp('created_at').defaultTo(knex.fn.now())
+        t.string('pickup_time')
         t.boolean('completed')
         t.boolean('paid')
     })
-  
+
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema.dropTable('orders')
-  
+
 };
