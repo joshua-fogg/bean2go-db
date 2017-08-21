ce64de653e2a9ed1119117c767132b6a40358230
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 16:17:07 +1200

    add coffees to coffeee_list

--- seeds/coffee_list.js
+++ seeds/coffee_list.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('coffee_list').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('coffee_list').insert([
+        {id: 1, coffee_type: 'Flat White', size: 'S', price: '4.20'},
+        {id: 2, coffee_type: 'Flat White', size: 'M', price: '4.50'},
+        {id: 3, coffee_type: 'Flat White', size: 'L', price: '5.00'},
+        {id: 4, coffee_type: 'Cappuccino', size: 'S', price: '4.20'},
+        {id: 5, coffee_type: 'Cappuccino', size: 'M', price: '4.50'},
+        {id: 6, coffee_type: 'Cappuccino', size: 'M', price: '5.00'},
+        {id: 7, coffee_type: 'Long Black', size: 'S', price: '3.80'},
+        {id: 8, coffee_type: 'Short Black', size: 'XS', price: '3.80'},
+        {id: 9, coffee_type: 'Versato', size: 'S', price: '4.20'},
       ]);
     });
 };
