62fbb28796244b13aaf29caffadeb7267bc13164
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 12:08:46 +1200

    Create coffee_list schema

--- migrations/20170821115950_coffee_list.js
+++ migrations/20170821115950_coffee_list.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.creatTableIfNotExists('coffee_list',(t) => {
+        t.increments('id')
+        t.string('coffee_type'),
+        t.string('size'),
+        t.string('price')
+    }) 
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('coffee_list')
   
 };
