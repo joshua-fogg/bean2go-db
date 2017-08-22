9cf0ae64c237aee50ebf961d5c0c09c018be453f
Joshua <joshuafoggnz@gmail.com>
2017-08-22 15:15:28 +1200

    add budy parser add module.exports

--- server/api-routes.js
+++ server/api-routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const config = require('../knexfile').development
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 const db = require('./db')
 
 router.use(bodyParser.json())
 
+// router.get('/getlist', (req, res) => {})
 
-
-router.get('/getlist', (req, res) => {})
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 var path = require('path')
 var express = require('express')
+
+var apiRoutes = require('./api-routes') // directing to api-routes for api services.
 var server = express()
 
-
+server.use(express.static(path.join(__dirname, '../public'))) // setting server public folder
 const PORT = process.env.PORT || 3000 // establish PORT, add deployment port ||
-const apiRoutes = require('./api-routes') // directing to api-routes for api services.
 
 
 server.listen(PORT, function() {
     console.log('Listening on port', PORT)
 })
-server.use(express.static(path.join(__dirname, '../public'))) // setting server public folder
 
 
-server.use('/api/v1', apiRoutes) // parsing api calls to api-routes
-server.use('*', (req, res) => { // wildcard server route to send index && app
-    res.send('../public/index.html')
-})
+
+server.use('/api/v1/', apiRoutes) // parsing api calls to api-routes
+server.use('*', (req, res) => {
+        res.send('../public/index.html')
+    })
+    // wildcard server route to send index && app
